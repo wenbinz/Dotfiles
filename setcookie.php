e066c2d2447c093392808f4942ed66e6df052673
Wenbin Zhao <wenbin@ubuntu.(none)>
2013-04-04 00:00:10 -0700

    wrong commit earlier

--- setcookie.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-$Month = 84600 + time();
-//this adds a day to the current time
-setcookie("LastVisit", date("F jS -g:i a"), $Month);
-?>
--- update.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def write(num):
-  f = open("counter.txt","w")
-  f.write(str(num))
-  f.close()
-if  __name__=="__main__":
-  write(sys.argv[1])
